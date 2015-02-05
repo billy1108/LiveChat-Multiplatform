3c3caf122b845efab576ac49887dc71fe2daf0cc
billy.1108 <billy.11080@gmail.com>
2015-02-05 16:28:32 -0500

    parse message

--- ios/LiveChatIos/app/controller/BaseViewController.rb
+++ ios/LiveChatIos/app/controller/BaseViewController.rb
@@ -12,9 +12,18 @@ class BaseViewController < UIViewController
     SIOSocket.socketWithHost("http://localhost:3000", response: lambda {  |socket|
       @socket = socket
       p "#{@socket.inspect}"
+      setupSocketListener
     })
-  
+    
+    
     self.navigationController.navigationBar.translucent = false
+  end
+
+  def setupSocketListener
+    @socket.on("new message", callback: lambda{ |data|
+      msg = Message.new(data.first["username"], data.first["message"])
+      p msg
+    })
   end
 
 
