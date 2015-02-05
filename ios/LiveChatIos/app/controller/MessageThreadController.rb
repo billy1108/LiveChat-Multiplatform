04f8a4c10a09a2dd2f509087a32f6ad25eb497b7
DeluxeAlonso <alonso.alvarez@tektonlabs.com>
2015-02-05 13:45:49 -0500

    Message thread

--- ios/LiveChatIos/app/controller/MessageThreadController.rb
+++ ios/LiveChatIos/app/controller/MessageThreadController.rb
@@ -12,9 +12,15 @@ class MessageThreadController < UIViewController
   def viewDidLoad
     super
     self.navigationController.navigationBar.translucent = false
+    setup_elements
   end
 
+  def setup_elements
+    p "edfwsd"
+    socketIO = SocketIO.alloc.initWithDelegate(self)
+    # socketIO.connectToHost("localhost",onPort:nil)
 
+  end
 
   #MARK - ACTIONS
 
